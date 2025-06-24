46fee5f7337b0333e133a68505ba39faa69030a2
Jefino <jefinojacob9488@gmail.com>
2025-06-24 14:29:15 +0530

    Update recompile.sh

--- recompile.sh
+++ recompile.sh
@@ -8,11 +8,10 @@ fi
 directory=$1
 api_level=$2
 
-# Define the decompiled directory consistent with decompile.sh
 directory_decompile="${directory}_decompile"
 
 if [ -d "$directory_decompile/classes" ]; then
-    java -jar tools/smali.jar a -a "$api_level" "$directory_decompile/classes" -o "$directory/classes.dex"
+    java -cp tools/smali.jar org.jf.smali.Main a -a "$api_level" "$directory_decompile/classes" -o "$directory/classes.dex"
     echo "Recompiled $directory_decompile/classes/ to $directory/classes.dex"
 else
     echo "$directory_decompile/classes directory not found, skipping recompilation."
@@ -20,7 +19,7 @@ fi
 
 for i in {2..5}; do
     if [ -d "$directory_decompile/classes$i" ]; then
-        java -jar tools/smali.jar a -a "$api_level" "$directory_decompile/classes$i" -o "$directory/classes$i.dex"
+        java -cp tools/smali.jar org.jf.smali.Main a -a "$api_level" "$directory_decompile/classes$i" -o "$directory/classes$i.dex"
         echo "Recompiled $directory_decompile/classes$i/ to $directory/classes$i.dex"
     else
         echo "$directory_decompile/classes$i directory not found, skipping recompilation."
@@ -29,9 +28,9 @@ done
 
 if [ -d "$directory" ]; then
     cd "$directory" || exit 1
-    7z a -tzip "../${directory}_new.zip" * || echo "Failed to create ${directory}_new.zip, but continuing"
+    7z a -tzip "../${directory}_new.jar" * || echo "Failed to create ${directory}_new.jar, but continuing"
     cd .. || exit 1
-    echo "Created ${directory}_new.zip"
+    echo "Created ${directory}_new.jar"
 else
     echo "$directory not found, skipping JAR creation."
 fi
